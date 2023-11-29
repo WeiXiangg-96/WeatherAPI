8f78e0a54143692e46d86a70a73c1a457dd55739
Wei Xiang <weixiang.awx@gmail.com>
2023-11-29 16:42:57 +0800

    Completed Geocoding portion

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-const Weather_URL = "";
-const Location_URL = "http://api.openweathermap.org/geo/1.0/zip?" // zip={zip code},{country code}&appid={API key}";
-const apiToken = "";
+const Weather_URL = ""; // weather api
+const Location_URL = "http://api.openweathermap.org/geo/1.0/zip?" // location api
+const apiToken = "7d64ca693d79493070526648e07aba37";
 
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", async (req,res) => {
-    try {
-        const result = await axios.get(Location_URL + `zip=${req.body.zip},${req.body.country}&appid=${apiToken}`);
-        res.render("index.ejs", { secret: result.data.secret, user: result.data.username});
-    } catch (error) {
-        console.log(error.response.data);
-        res.status(500);
-    }
+app.get("/", (req,res) => {
+    res.render("index.ejs");
 });
 
 app.post("/result", async (req,res) => {
     try {
-        const result = await axios.get(API_URL + "");
-        res.render("index.ejs", { secret: result.data.secret, user: result.data.username});
+        const result = await axios.get(Location_URL + `zip=${req.body.zip},${req.body.country}&appid=${apiToken}`);
+        console.log(req.body.zip);
+        console.log(req.body.country);
+        res.render("index.ejs", {country: result.country, lat: result.lat, lon: result.lon});
     } catch (error) {
-        console.log(error.response.data);
+        console.log("Error");
         res.status(500);
     }
 });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
